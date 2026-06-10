1f0f93ba4757205c4b9a880578a293017b8417e4
Kian McGuinness <kian@Mac.lan>
2026-06-10 19:25:33 +0100

    Add initial implementation of logger service with MongoDB integration and data models

---
+++ logger-service/data/models.go
@@ -0,0 +1,144 @@
+package data
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models{
+	client = mongo
+	return Models{
+		LogEntry: LogEntry{},
+	}
+
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+	ID      string `bson:"_id,omitempty" json:"id,omitempty"`
+	Name    string `bson:"name,omitempty" json:"name,omitempty"`
+	Data    string `bson:"data,omitempty" json:"data,omitempty"`
+	CreatedAt time.Time `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt time.Time `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+}
+
+func (l *LogEntry) Insert(entry LogEntry) error {
+	collection := client.Database("logs").Collection("logs")
+	_, err := collection.InsertOne(context.TODO(), LogEntry{
+		Name:     entry.Name,
+		Data:     entry.Data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Error inserting log entry:", err)
+		return err
+	}
+	return nil
+}
+
+func (l *LogEntry) All() ([]*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	opts := options.Find().SetSort(bson.D{{Key: "created_at", Value: -1}})
+
+	cursor, err := collection.Find(context.TODO(), bson.M{}, opts)
+	if err != nil {
+		log.Println("Error finding log entries:", err)
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+
+	var logs []*LogEntry
+	for cursor.Next(ctx) {
+		var entry LogEntry
+		
+		err := cursor.Decode(&entry)
+		if err != nil {
+			log.Println("Error decoding log entry:", err)
+			return nil, err
+		}
+		logs = append(logs, &entry)
+	}
+
+	return logs, nil
+}
+
+func (l *LogEntry) FindByID(id string) (*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println("Error converting ID to ObjectID:", err)
+		return nil, err
+	}
+
+	var entry LogEntry
+	err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&entry)
+	if err != nil {
+		log.Println("Error finding log entry by ID:", err)
+		return nil, err
+	}
+	return &entry, nil
+}
+
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	if err := collection.Drop(ctx); err != nil {
+		log.Println("Error dropping log collection:", err)
+		return err
+	}
+	return nil
+}
+
+func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docID, err := primitive.ObjectIDFromHex(l.ID)
+	if err != nil {
+		log.Println("Error converting ID to ObjectID:", err)
+		return nil, err
+	}
+	result, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": docID},
+		bson.D{
+			{"$set", bson.D{
+				{"name", l.Name},
+				{"data", l.Data},
+				{"updated_at", time.Now()},
+			}},
+		},
+	)
+	if err != nil {
+		log.Println("Error updating log entry:", err)
+		return nil, err
+	}
+	return result, nil
+}
